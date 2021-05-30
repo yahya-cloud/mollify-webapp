7a340051eeb29779f8e5d42e4debe29ad53f51b7
yahya-cloud <59670962+yahya-cloud@users.noreply.github.com>
2021-05-30 18:25:12 +0530

    api commit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import * as axios from 'axios';
 
-const API = axios.create({baseURL: 'http://localhost:5000' });
+const API = axios.create({baseURL: 'https://nameless-beyond-33226.herokuapp.com' });
 
 API.interceptors.request.use((req) => {
 

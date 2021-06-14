b162a13da51bc2ef02e55ff31fe71bd08105258f
yahya-cloud <59670962+yahya-cloud@users.noreply.github.com>
2021-06-14 22:55:51 +0530

    commit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import * as axios from 'axios';
 
-const API = axios.create({baseURL: 'http://localhost:5000/' });
+const API = axios.create({baseURL: 'https://mollify-webapp.herokuapp.com/' });
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('userToken')){
